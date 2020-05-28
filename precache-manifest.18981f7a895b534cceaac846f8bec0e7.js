d9af341eaec91fd0192a66356140f51475cda752
Urmila <urmila.m.satpute1@gmail.com>
2020-05-28 20:58:14 +0530

    Updates

---
+++ precache-manifest.18981f7a895b534cceaac846f8bec0e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b33b2342cff5c08e30a7b0c5997ccd5",
+    "url": "/HackerNews/index.html"
+  },
+  {
+    "revision": "12769cb67593a51ae438",
+    "url": "/HackerNews/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ee44bac867d4d55a7b42",
+    "url": "/HackerNews/static/js/2.c1b17d3a.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/HackerNews/static/js/2.c1b17d3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12769cb67593a51ae438",
+    "url": "/HackerNews/static/js/main.e1305bc6.chunk.js"
+  },
+  {
+    "revision": "6edaa396712e1b41b75d",
+    "url": "/HackerNews/static/js/runtime-main.425fc1e4.js"
+  }
+]);
