0dfa9e06202f4cc6f0733ba32c76605ea69ff962
Urmila <urmila.m.satpute1@gmail.com>
2020-05-28 21:09:14 +0530

    addedDeployScipt

--- src/Services/HackerNewsAPI.js
+++ src/Services/HackerNewsAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseurl="http://hn.algolia.com/api/v1/";
+export const baseurl="https://hn.algolia.com/api/v1/";
 export const AllStoriesUrl=`${baseurl}search?query=foo&tags=story`;
 
 export const AllStories= async (page)=>{
